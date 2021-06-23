690e0a56707115fba2e5b0651a30b01582841e11
Susino <t.sygut@clickray.pl>
2021-06-23 10:55:46 +0200

    heroku deploy

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const connectDB = require("./config/db");
 const path = require("path");
 
 // Step 1: 
-app.use(express.static(path.resolve(__dirname,"./client/build")));
+app.use(express.static("client/build"));
 // Step 2: 
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
-
+  response.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+}); 
+ 
 connectDB();
 
 const app = express();
