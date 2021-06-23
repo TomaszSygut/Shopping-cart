7318a68db4b8532caf667b2babbdc00ecb2c3f15
Susino <t.sygut@clickray.pl>
2021-06-23 10:42:17 +0200

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ const connectDB = require("./config/db");
 const path = require("path");
 
 // Step 1: 
-app.use(express.static(path.resolve(__dirname,"../client/build")));
+app.use(express.static(path.resolve(__dirname,"./client/build")));
 // Step 2: 
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 connectDB();
@@ -17,7 +17,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-
+ 
 app.get("/", (req, res) => { 
   res.json({ message: "API running..." });
 });
