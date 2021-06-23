d81ec2ce6374437846aa133a69511e156be98555
Susino <t.sygut@clickray.pl>
2021-06-23 11:26:47 +0200

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,29 @@
 require("dotenv").config();
 const express = require("express");
+const app = express();
 const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
-
 const path = require("path");
 
-// Step 1: 
-app.use(express.static("client/build"));
-// Step 2: 
+  
+
+if(process.env.NODE_ENV === 'production') { 
+app.use(express.static('../client/build'));
+
+app.get('*', (req,res) => {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+}
+ 
+// Step 2:   
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-}); 
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});  
  
 connectDB();
 
-const app = express();
 
+ 
 app.use(express.json());
  
 app.get("/", (req, res) => { 
