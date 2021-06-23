97b484b8ad686085995db26cd3aa2eb71411ce64
Susino <t.sygut@clickray.pl>
2021-06-23 10:27:49 +0200

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,15 @@ const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
 
+const path = require("path");
+
+// Step 1: 
+app.use(express.static(path.resolve(__dirname, "/Users/mac-113/Desktop/Shopping-cartt/frontend/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "/Users/mac-113/Desktop/Shopping-cartt/frontend/build", "index.html"));
+});
+
 connectDB();
 
 const app = express();
